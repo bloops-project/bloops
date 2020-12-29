e8866ff4617e3f21da360db36bf0fd64b259f906
robotomize <robotomize@gmail.com>
2020-12-29 17:51:12 +1100

    fix messages

--- internal/bloopsbot/resource/version.go
+++ internal/bloopsbot/resource/version.go
@@ -2,7 +2,7 @@ package resource
 
 const (
 	ProjectName    = "bloopsbot"
-	ProjectVersion = "v1.7.11"
+	ProjectVersion = "v1.7.12"
 	Graffiti       = "___.   .__                                 ___.           __   \n\\_ |__ |  |   ____   ____ ______  ______   \\_ |__   _____/  |_ \n | __ \\|  |  /  _ \\ /  _ \\\\____ \\/  ___/    | __ \\ /  _ \\   __\\\n | \\_\\ \\  |_(  <_> |  <_> )  |_> >___ \\     | \\_\\ (  <_> )  |  \n |___  /____/\\____/ \\____/|   __/____  >____|___  /\\____/|__|  \n     \\/                   |__|       \\/_____/   \\/             \n\n"
 	GreetingCLI    = "%s %s\ntg: %s\ngithub: %s\n"
 	GithubBloopUrl = "https://github.com/robotomize/bloopsbot.git"
